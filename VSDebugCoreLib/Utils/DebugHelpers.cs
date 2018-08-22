7b6608b323928f28f4eae7ae77b9b0b7b1de7d77
Ovidiu Ionescu <ovidiu@vsdebug.pro>
2018-08-22 03:05:17 +0300

    [+] Added function to check if process is minidump

--- VSDebugCoreLib/Utils/DebugHelpers.cs
+++ VSDebugCoreLib/Utils/DebugHelpers.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using EnvDTE;
 
 namespace VSDebugCoreLib.Utils
 {
-    class DebugHelpers
+    public class DebugHelpers
     {
+
+        public static bool IsMiniDumpProcess(Process process)
+        {
+            string strExt = Path.GetExtension(process.Name.ToLower());
+
+            if (".dmp" == strExt)
+                return true;
+
+            return false;
+        }
     }
 }
