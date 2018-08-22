5e1d51e5a0481a03697fce663fcfaa94bb6aeebe
Ovidiu Ionescu <ovidiu@vsdebug.pro>
2018-08-22 03:04:40 +0300

    [+] Added DkmMethods

---
+++ VSDebugCoreLib/Utils/DkmMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Debugger;
+using Microsoft.VisualStudio.Debugger.CallStack;
+
+namespace VSDebugCoreLib.Utils
+{
+    public class DkmMethods
+    {
+        public static DkmProcess GetDkmProcess(StackFrame stackFrame)
+        {
+            if(null != stackFrame)
+            {
+                DkmStackFrame dkmStackFrame = DkmStackFrame.ExtractFromDTEObject(stackFrame);
+                return dkmStackFrame.Process;
+            }
+
+            return null;
+        }
+    }
+}
