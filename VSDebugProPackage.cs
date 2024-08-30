5b7ce9b1e2b817f3cdd4ad428f714eaf6b71ddd6
Ovidiu Ionescu <ovidiuvio@gmail.com>
2024-08-30 19:47:05 +0300

    Implement AsyncPackage

--- VSDebugProPackage.cs
+++ VSDebugProPackage.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.Design;
+﻿using System;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
@@ -11,51 +13,37 @@ using VSDebugCoreLib.UI.Tools;
 
 namespace VSDebugPro
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID =
-        400)] // This attribute is used to register the information needed to show this context
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // This attribute is used to register the information needed to show this context
     [ProvideAutoLoad("1336C085-E903-4333-BF29-7D4DF860DF9F")] // Force auto load
     [ProvideToolWindow(typeof(ConsoleWindow))] // console window
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.GuidVSDebugProPkgString)]
-    public sealed class VSDebugProPackage : Package
+    public sealed class VSDebugProPackage : AsyncPackage
     {
-        /// <summary>
-        ///     Default constructor of the context.
-        /// </summary>
+        public static VSDebugContext Context { get; private set; }
+        public Assembly VSDAssembly { get; }
+
         public VSDebugProPackage()
         {
             VSDAssembly = Assembly.GetExecutingAssembly();
         }
 
-        public static VSDebugContext Context { get; private set; }
-
-        public Assembly VSDAssembly { get; }
-        /////////////////////////////////////////////////////////////////////////////
-        // Overridden Context Implementation
-
-        #region Context Members
-
-        /// <summary>
-        ///     Initialization of the context; this method is called right after the context is sited, so this is the place
-        ///     where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        protected override void Initialize()
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
             Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Initialized: {0}", ToString()));
 
-            // Initialize base class
-            base.Initialize();
-
-            // Create extension context, this is used as global var for various stuff
+            // Create extension context
             Context = new VSDebugContext(this, VSDAssembly)
             {
-                IDE = (DTE2) GetService(typeof(DTE)),
-                MenuCommandService = GetService(typeof(IMenuCommandService)) as OleMenuCommandService
+                IDE = (DTE2)await GetServiceAsync(typeof(DTE)),
+                MenuCommandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService
             };
             Context.Initialize();
         }
-
-        #endregion Context Members
     }
 }
